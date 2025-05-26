b00e5a824f1ae293138520424886b9da779f6517
ratnaphp <ra7709702@gmail.com>
2025-05-26 16:52:05 +0600

    last

--- update.php
+++ update.php
@@ -1,5 +1,5 @@
 <?php
-include 'config.php';
+/*include './config.php';
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $id    = $_POST['id'];
@@ -17,5 +17,47 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
 } else {
     echo "Invalid request.";
+}*/
+
+
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "student_management";
+$is_connect = false;
+
+
+$connection = mysqli_connect($servername, $username, $password, $dbname);
+ 
+require_once './config.php';
+
+if ($connection) {
+    $is_connect = true;
+}else {
+    echo "Connected";
 }
+$name = $_POST['name'];
+$email = $_POST['e-mail'];
+$phone = $_POST['phone'];
+$address = $_POST['address'];
+ 
+
+if ($is_connect)
+
+    $sql = "UPDATE students 
+    
+    SET UPDATE `students` SET `ID`='[value-1]',`Name`='[value-2]',`e-mail`='[value-3]',`Phone`='[value-4]',`Address`='[value-5]' WHERE $ID";
+
+     if ($connection->query($sql) === TRUE) {
+    header("index.php")
+     } else {
+        echo "Error: " . $sql . "<br>" . $connection->error;
+
+     }
+    
+
+
+
+
 ?>
